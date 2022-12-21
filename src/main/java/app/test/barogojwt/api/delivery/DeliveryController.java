1695f6fccbd34993c31caab5a9cce72f1a5349af
jungyonge <jungyong_e@naver.com>
2022-12-21 16:04:22 +0900

    delivery 조회 개발

--- src/main/java/app/test/barogojwt/api/delivery/DeliveryController.java
+++ src/main/java/app/test/barogojwt/api/delivery/DeliveryController.java
@@ -1,10 +1,18 @@
 package app.test.barogojwt.api.delivery;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/delivery")
 public class DeliveryController {
+
+    @GetMapping
+    @Secured({"ROLE_NORMAL_USER"})
+    public String getDelivery(){
+        return "OK";
+    }
 
 }
